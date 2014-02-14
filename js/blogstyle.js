8ce1209bd6c89f8efedffc7150b36176768b91bc
wenin819 <wenin819@gmail.com>
2014-02-14 23:20:14 +0800

    增加google代码语法高亮

--- js/blogstyle.js
+++ js/blogstyle.js
@@ -1,3 +1,5 @@
 $(function(){
 	$('table').addClass('table table-striped table-bordered table-hover table-condensed');
+	$('code').addClass('prettyprint linenums');
+	$.getScript('https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js?skin=sons-of-obsidian');
 });
